bb80e85e6247fa618d0a8e1ab350910b4b2cb418
yangmxtim <58218293+yangmxtim@users.noreply.github.com>
2021-12-28 22:12:31 +0800

    make 3 usermode without iconbuttom  and correct link

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -2,7 +2,7 @@ import _axios from "axios";
 
 const axios = () => {
     const instance = _axios.create({
-        baseURL: "http://localhost:8080",
+        baseURL: "http://54.65.248.67:8080/",
         timeout: 5000
     });
     return instance;
--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,7 +1,8 @@
 import './Topbar.css';
 import React from 'react';
+import axios from "../Axios";
 import { useState } from 'react';
-import {Button}  from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from './SearchBar';
@@ -10,11 +11,22 @@ import RegisterModal from './RegisterModal';
 import LogoutButton from './LogoutButton';
 
 
+var ID;
+var Username;
 const Topbar = () => {
-  const navigate = useNavigate();
-  const [isShow,setIsShow] = useState(false);
+  axios.post("/authorize", { withCredentials: true })
+    .then((response) => {
+      alert(response.data);
+      ID = response.data;
+    }
+    );
 
-  const [isShow_R,setIsShow_R] = useState(false);
+  ID = "member";
+
+  const navigate = useNavigate();
+  const [isShow, setIsShow] = useState(false);
+
+  const [isShow_R, setIsShow_R] = useState(false);
 
   const handleShow = () => setIsShow(true);
   const handleClose = () => setIsShow(false);
@@ -22,31 +34,83 @@ const Topbar = () => {
   const handleRegisterShow = () => setIsShow_R(true);
   const handleRegisterClose = () => setIsShow_R(false);
 
-
-  // const handleRegisterClick = () => navigate('/register');
-
-  return (
-    <div className='topbar'>
-        <div className='title' onClick={()=> navigate('/')}>ShrimPC</div>
+  if (ID === "Fail!") {
+    return (
+      <div className='topbar'>
+        <div className='title' onClick={() => navigate('/')}>ShrimPC</div>
 
         <SearchBar />
-		
-        <div className='memberService'> 
-            <Button className='redColorNoBorder' onClick={handleRegisterShow}>註冊</Button>
-            <div className='seperator'></div>
-            <Button className='redColorNoBorder' onClick={handleShow}>登入</Button>
-            {/*<LogoutButton />*/}
-			<div className='seperator'></div>
-			<Button className='redColorNoBorder' onClick={()=> navigate('/Cart')}>購物車</Button>
+
+        <div className='memberService'>
+          <Button className='redColorNoBorder' onClick={handleRegisterShow}>註冊</Button>
+          <div className='seperator'></div>
+          <Button className='redColorNoBorder' onClick={handleShow}>登入</Button>
+          {/*<LogoutButton />*/}
+          <div className='seperator'></div>
+          <Button className='redColorNoBorder' onClick={() => navigate('/Cart')}>購物車</Button>
+
         </div>
 
-        <LoginModal isShow = {isShow} handleClose = {handleClose}/>
-        <RegisterModal isShow_R = {isShow_R} handleRegisterClose = {handleRegisterClose}/>
+        <LoginModal isShow={isShow} handleClose={handleClose} />
+        <RegisterModal isShow_R={isShow_R} handleRegisterClose={handleRegisterClose} />
 
-    </div>
-    
-  )
-  
+      </div>
+
+    )
+  }
+  if (ID === "admin") {
+    return (
+      <div className='topbar'>
+        <div className='title' onClick={() => navigate('/')}>ShrimPC</div>
+
+        <SearchBar />
+
+        <div className='memberService'>
+          {/* <Button className='redColorNoBorder' onClick={handleRegisterShow}>註冊</Button>
+              <div className='seperator'></div> */}
+          <Button className='redColorNoBorder' onClick={handleShow}>ADMIN</Button> {/*  Need IconButton */}
+          {/* 不確定這邊的要跳去哪裡 */}
+          {/*<LogoutButton />*/}
+          <div className='seperator'></div>
+          <Button className='redColorNoBorder' onClick={() => navigate('/Cart')}>SETTING</Button>  {/*  Need IconButton */}
+          {/* 不確定這邊的要跳去哪裡 */}
+        </div>
+
+        <LoginModal isShow={isShow} handleClose={handleClose} />
+        <RegisterModal isShow_R={isShow_R} handleRegisterClose={handleRegisterClose} />
+
+      </div>
+
+    )
+  }
+  if (ID === "member") {
+    axios.post("/member/requestMemberInfo", { withCredentials: true })
+    .then((response) => {
+      Username = response.username;
+    })
+    return (
+      <div className='topbar'>
+        <div className='title' onClick={() => navigate('/')}>ShrimPC</div>
+
+        <SearchBar />
+
+        <div className='memberService'>
+          {/* <Button className='redColorNoBorder' onClick={handleRegisterShow}>註冊</Button>
+              <div className='seperator'></div> */}
+          <Button className='redColorNoBorder' onClick={handleShow}>123{Username}</Button>
+          {/* 這裡應該是跳去InfoPage? */}
+          {/*<LogoutButton />*/}
+          <div className='seperator'></div>
+          <Button className='redColorNoBorder' onClick={() => navigate('/Cart')}>CART</Button> {/*  Need IconButton */}
+        </div>
+
+        <LoginModal isShow={isShow} handleClose={handleClose} />
+        <RegisterModal isShow_R={isShow_R} handleRegisterClose={handleRegisterClose} />
+
+      </div>
+
+    )
+  }
 };
 
 export default Topbar;
