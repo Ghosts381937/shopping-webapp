58b3eba2e34c5e1f3ef3b7a5a6e6e0d5fb170b24
Ghosts381937 <she.xing88@gmail.com>
2022-01-01 11:45:23 +0800

    Fix cart table

--- src/components/Topbar/Cart.js
+++ src/components/Topbar/Cart.js
@@ -53,46 +53,46 @@ const Cart = () => {
 	return (
 		<div className="container">
 			<div className="cart">
-
 				<table>
-					<tr className="collection-item avatar">
-						<th colspan="2"><center>商品</center></th>
-						<th><center>單價</center></th>
-						<th><center>數量</center></th>
-						<th><center>操作</center></th>
-					</tr>
-					{item.map((product) => (
-						<tr className="collection-item avatar" key={product.id}>
-							<td className="item-img">
-								<img src={"data: image; base64," + product.image} alt={product.name} className="" />
-							</td>
-							<td className="item-title">
-								<span className="title">{product.name}</span>
-							</td>
-							<td className="item-price">
-								<p><b>{product.price}</b></p>
-							</td>
-							<td className="item-desc">
-								<p><b>{product.quantity}</b></p>
-							</td>
-							<td className="item-desc">
-								
-								<Button variant="contained" onClick={() => { handleRemove(product.id) }}>取消</Button>
-							</td>
+					<tbody>
+						<tr className="collection-item avatar">
+							<th colspan="2"><center>商品</center></th>
+							<th><center>單價</center></th>
+							<th><center>數量</center></th>
+							<th><center>操作</center></th>
 						</tr>
+						{item.map((product) => (
+							<tr className="collection-item avatar" key={product.id}>
+								<td className="item-img">
+									<img src={"data: image; base64," + product.image} alt={product.name} className="" />
+								</td>
+								<td className="item-title">
+									<span className="title">{product.name}</span>
+								</td>
+								<td className="item-price">
+									<p><b>{product.price}</b></p>
+								</td>
+								<td className="item-desc">
+									<p><b>{product.quantity}</b></p>
+								</td>
+								<td className="item-desc">
 
-					))}
-					<tr className="collection-item avatar">
-						<td colspan="2">總共</td>
-						<td className="item-price">
-							{PR}
-						</td>
-						<td className="item-desc">
-							{QT}
-						</td>
-						<td className="item-desc"><Button variant="outlined" onClick={() => { alert("Success") }}>結帳</Button></td>
-					</tr>
+									<Button variant="contained" onClick={() => { handleRemove(product.id) }}>取消</Button>
+								</td>
+							</tr>
 
+						))}
+						<tr className="collection-item avatar">
+							<td colspan="2">總共</td>
+							<td className="item-price">
+								{PR}
+							</td>
+							<td className="item-desc">
+								{QT}
+							</td>
+							<td className="item-desc"><Button variant="outlined" onClick={() => { alert("Success") }}>結帳</Button></td>
+						</tr>
+					</tbody>
 				</table>
 			</div>
 
